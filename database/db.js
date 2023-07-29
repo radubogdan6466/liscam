d67fe895fef584a51948dfd9aef80efdefd97a99
radubogdan6466 <radubogdan6466@gmail.com>
2023-07-29 11:04:04 +0300

    second

--- database/db.js
+++ database/db.js
@@ -5,7 +5,7 @@ const Connection = async (username, password) => {
 
   try {
     await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
-    console.log("Conectat cu bd");
+    console.log("Conectatt cu bd");
   } catch (error) {
     console.log("Naspa, eroare la conectare cu bd", error);
   }
